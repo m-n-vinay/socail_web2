53572a5fbc3d692f84a65683c89d39d8aa121af0
vinay <vinay@users.noreply.github.com>
2022-05-07 10:28:36 +0530

    posts in home page

--- socail_web2/Controllers/HomeController.cs
+++ socail_web2/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using socail_web2.Models;
 using System.Diagnostics;
+using socail_web2.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace socail_web2.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            return View(await _context.Post.ToListAsync());
         }
 
         public IActionResult Privacy()
